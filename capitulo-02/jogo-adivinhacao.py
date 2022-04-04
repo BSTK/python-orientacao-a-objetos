93088b1fe385b32fbdfd87cc5241c8074ba4b7bc
Bruno Luz <bsilva.se@gmail.com>
2022-04-04 13:31:54 -0300

    main - Capitulo-2: Melhorando o jogo da adivinhação

--- capitulo-02/jogo-adivinhacao.py
+++ capitulo-02/jogo-adivinhacao.py
@@ -1,34 +1,50 @@
+import os
+import time
+
 NUMERO_SECRETO = 42
 
 
 def main():
-    print('\n')
-    print('\t*******************************')
-    print('\t**     JOGO DA ADVINHAÇÃO    **')
-    print('\t*******************************')
-    print('\n')
+    total_tentativas = 3
+    numero_da_rodada = 1
 
-    chute_numero_informado = int(input('\tTente advinhar o número: '))
-    print('\tNúmero informado: {}'.format(chute_numero_informado))
+    while numero_da_rodada <= total_tentativas:
+        print('\n')
+        print('\t*******************************')
+        print('\t**     JOGO DA ADVINHAÇÃO    **')
+        print('\t*******************************')
+        print('\n')
 
-    acertou_numero_secreto = NUMERO_SECRETO == chute_numero_informado
-    numero_secreto_eh_maior = NUMERO_SECRETO > chute_numero_informado
-    numero_secreto_eh_menor = NUMERO_SECRETO < chute_numero_informado
+        print('\t-------- RODADA: {} -----------'.format(numero_da_rodada))
+        print('\n')
 
-    print('\n')
+        chute_numero_informado = int(input('\tTente advinhar o número: '))
+        print('\tNúmero informado: {}'.format(chute_numero_informado))
 
-    if acertou_numero_secreto:
-        print('\t*** ACERTOOOUUUu!! ***')
+        acertou_numero_secreto = NUMERO_SECRETO == chute_numero_informado
+        numero_secreto_eh_maior = NUMERO_SECRETO > chute_numero_informado
+        numero_secreto_eh_menor = NUMERO_SECRETO < chute_numero_informado
 
-    if numero_secreto_eh_maior:
-        print('\t)-: )-: ErrrooouuuU!! Número secreto é -> MAIOR que {}'
-              .format(chute_numero_informado))
+        print('\n')
 
-    if numero_secreto_eh_menor:
-        print('\t)-: )-: ErrrooouuuU!! Número secreto é -> MENOR que {}'
-              .format(chute_numero_informado))
+        if acertou_numero_secreto:
+            print('\t*** ACERTOOOUUUu!! ***')
+            break
 
-    print('\n')
+        if numero_secreto_eh_maior:
+            print('\t)-: )-: ErrrooouuuU!! Número secreto é -> MAIOR que {}'
+                  .format(chute_numero_informado))
+            numero_da_rodada += 1
+            time.sleep(2)
+
+        if numero_secreto_eh_menor:
+            print('\t)-: )-: ErrrooouuuU!! Número secreto é -> MENOR que {}'
+                  .format(chute_numero_informado))
+            numero_da_rodada += 1
+            time.sleep(2)
+
+        print('\n')
+        os.system('clear')
 
 
 if __name__ == '__main__':
