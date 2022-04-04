ed868f74d39ac3b4571952dde1993d43fe4aad4a
Bruno Luz <bsilva.se@gmail.com>
2022-04-04 20:07:53 -0300

    main - Capitulo-5: Tuplas e Conjuntos

---
+++ capitulo-05/tuplas_conjuntos.py
@@ -0,0 +1,35 @@
+def main():
+    dias_de_semana = (
+        'Domingo',
+        'Segunda',
+        'Terça',
+        'Quarta',
+        'Quinta',
+        'Sexta',
+        'Sabado',
+    )
+
+    meses_do_ano = (
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro'
+    )
+
+    vogais = {'A', 'E', 'I', 'O', 'U'}
+
+    print('Dias da semana: {}'.format(dias_de_semana))
+    print('Mses do ano: {}'.format(meses_do_ano))
+    print('Vogais: {}'.format(vogais))
+
+
+if __name__ == '__main__':
+    main()
